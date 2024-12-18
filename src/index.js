0fb842a14de6b17de34fe3ebf3a5c2622031e9f7
PalesaShiluvane <Palesashiluvane@gmail.com>
2024-12-18 18:17:21 +0200

    Updated the searchSubmit, searchCity and refreshWeather functions

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#current-temp");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
 //API
 
-function showCityDegree(response) {
-  let temperatureElement = document.querySelector("#current-temp");
-  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = response.data.city;
-}
-
-//Display Search Input
-function SearchCity(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-input");
-  let city = searchInput.value;
-
+function searchCity(city) {
   let apiKEY = `90837b7722d3o37ab2424dfa2715bt14`;
   let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKEY}&units=metric`;
-  axios.get(apiURL).then(showCityDegree);
+
+  axios.get(apiURL).then(refreshWeather);
 }
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", SearchCity);
+
+//Display Search Input as City
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
+}
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
 //Current Date
 function formatDate(date) {
   let day = date.getDay();
